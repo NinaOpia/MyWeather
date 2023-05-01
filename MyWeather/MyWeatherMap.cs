1b12672c19ed41a82196856eb939563b7a22eabd
Nina <boldurfaith@gmail.com>
2023-04-30 23:17:01 -0700

    My Final Project

---
+++ MyWeather/MyWeatherMap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace MyWeather
+{
+	public class MyWeatherMap
+	{
+		public static double GetTemp(string weatherUrl)
+		{
+			var client = new HttpClient();
+
+			var response = client.GetStringAsync(weatherUrl).Result;
+
+			var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+
+			return temp;
+		}
+
+		public static double GetHumidity(string weatherUrl)
+		{
+			var client = new HttpClient();
+
+			var response = client.GetStringAsync(weatherUrl).Result;
+
+			var humidity = double.Parse(JObject.Parse(response)["main"]["humidity"].ToString());
+
+			return humidity;
+		}
+    }
+}
+
---
+++ MyWeather/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using MyWeather;
+using Newtonsoft.Json.Linq;
+
+string key = File.ReadAllText("appsettings.json");
+string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+Console.WriteLine("Enter your zipcode");
+var zipCode = Console.ReadLine();
+
+//Call for the string, that is, the Endpoint. In other words, the api call and in this case
+//I used "weatherUrl" to represent the weatherMap url.
+var weatherUrl = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIKey}";
+
+Console.WriteLine();
+
+Console.WriteLine($"At the moment, the temp is {MyWeatherMap.GetTemp(weatherUrl)}°F in your location. ");
+Console.WriteLine();
+
+Console.WriteLine($"While the humidity is {MyWeatherMap.GetHumidity(weatherUrl)}°F. ");
+Console.WriteLine();
+
+Console.WriteLine($"Thank you for checking in, goodbye!");
+Console.WriteLine();
